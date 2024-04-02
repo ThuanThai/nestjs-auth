84130374ce02a5b7f5faa86e42b46a53569f1aa8
ThaiThuan <s3877024@rmit.edu.vn>
2024-04-02 14:00:40 +0700

    fix project

--- src/stateful/stateful.controller.ts
+++ src/stateful/stateful.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller } from "@nestjs/common";
 
-@Controller('stateful')
+@Controller("stateful")
 export class StatefulController {}
--- src/stateless/stateless.controller.ts
+++ src/stateless/stateless.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './passport/stateless.local.guard';
-import { StatelessService } from './stateless.service';
-import { JwtAuthGuard } from './passport/stateless.jwt.auth.guard';
+import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
+import { LocalAuthGuard } from "./passport/stateless.local.guard";
+import { StatelessService } from "./stateless.service";
+import { JwtAuthGuard } from "./passport/stateless.jwt.auth.guard";
 
-@Controller('stateless')
+@Controller("stateless")
 export class StatelessController {
-    constructor(private statelessService: StatelessService) { }
+    constructor(private statelessService: StatelessService) {}
     @UseGuards(LocalAuthGuard)
-    @Post('login')
+    @Post("login")
     async login(@Request() req) {
         return this.statelessService.login(req.user);
     }
 
     @UseGuards(JwtAuthGuard)
-    @Get('user')
+    @Get("user")
     getProfile(@Request() req) {
         delete req.user.password;
         return req.user;
